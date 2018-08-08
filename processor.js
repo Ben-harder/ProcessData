a72d4056a5102febdf4f5b17f5540cd68ba2f84f
Ben <ben.harder2@gmail.com>
2018-08-08 14:55:37 -0700

    initial select folder code done

---
+++ processor.js
@@ -0,0 +1,30 @@
+// This file is required by index.html and will 
+// execute the data processing.
+
+const { ipcRenderer, remote } = require('electron');
+
+const { dialog } = remote;
+
+const form = document.querySelector('form');
+
+const inputs = {
+    fileType: form.querySelector('select[name="fileType"]'),
+    sourcePath: form.querySelector('input[name="sourcePath"]')
+};
+
+const buttons = {
+    source: document.getElementById('sourcePathButton'),
+    submit: form.querySelector('button[type="submit"]')
+};
+
+buttons.source.addEventListener('click', (event) =>
+{
+    event.preventDefault();
+    const directory = dialog.showOpenDialog({
+        properties: ['openDirectory'],
+    });
+    if (directory)
+    {
+        inputs.source.values = directory;
+    }
+});
