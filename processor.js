3462ae65d0fa8d06a82fc09c6d98bdac80a50daf
Ben <ben.harder2@gmail.com>
2018-08-15 09:29:36 -0700

    added destination folder

--- processor.js
+++ processor.js
@@ -9,11 +9,13 @@ const form = document.querySelector('form');
 
 const inputs = {
     fileType: form.querySelector('select[name="fileType"]'),
-    sourcePath: document.getElementById('sourcePath')
+    sourcePath: document.getElementById('sourcePath'),
+    outputPath: document.getElementById('outputPath')
 };
 
 const buttons = {
     source: document.getElementById('sourcePathButton'),
+    output: document.getElementById('outputPathButton'),
     submit: document.getElementById('submit')
 };
 
@@ -29,6 +31,17 @@ buttons.source.addEventListener('click', (event) =>
     }
 });
 
+buttons.output.addEventListener('click', (event) => {
+    event.preventDefault();
+    const directory = dialog.showOpenDialog({
+        properties: ['openDirectory'],
+    });
+    if (directory)
+    {
+        inputs.outputPath.value = directory;
+    }
+});
+
 buttons.submit.addEventListener('click', (event) =>
 {
     event.preventDefault();
@@ -36,14 +49,16 @@ buttons.submit.addEventListener('click', (event) =>
     // Format the sourcePath to have a '\' at the end
     if (!inputs.sourcePath.value.endsWith('\\'))
         inputs.sourcePath.value = inputs.sourcePath.value + '\\';
-
+    else if (!inputs.outputPath.value.endsWith('\\'))
+        inputs.outputPath.value = inputs.outputPath.value + '\\';
+    
     fs.readdir(inputs.sourcePath.value, (err, files) =>
     {
-        handleFiles(files, inputs.sourcePath.value);
+        handleFiles(files, inputs.sourcePath.value, inputs.outputPath.value);
     });
 });
 
-function handleFiles(files, sourcePath)
+function handleFiles(files, sourcePath, outputPath)
 {
 
     // The first file's header will be used as a template to check all following files.
@@ -53,25 +68,25 @@ function handleFiles(files, sourcePath)
     {
         var f = files[i];
 
-        if (!header.match(getHeader(sourcePath + f)))
+        if (!header.match(getHeader(sourcePath + f))) // If there is a header mismatch then append that file name to the mismatch log.
         {
             console.log("File header mismatch between " + files[0] + " and " + f);
-            appendMismatch(files[0], f, sourcePath);
+            appendMismatch(files[0], f, outputPath);
         }
-        else 
+        else // Otherwise the headers match so append the file data to the output file.
         {
-            var data = removeHeader(sourcePath + f);
-            appendFile(data, sourcePath);
+            var data = removeHeader(sourcePath + f, header);
+            appendFile(data, outputPath);
         }
     }
 }
 
 // This function will append the passed file data to an output aggregated file.
-function appendFile(data, sourcePath)
+function appendFile(data, path)
 {
     try
     {
-        fs.appendFileSync(sourcePath + "aggregatedData.txt", data, 'utf8');
+        fs.appendFileSync(path + "aggregatedData.txt", data, 'utf8');
     } catch (err)
     {
         // Get rid of old alert
@@ -87,13 +102,13 @@ function appendFile(data, sourcePath)
 }
 
 // This function will create a text file which will have the file header mismatches
-function appendMismatch(file1, file2, sourcePath)
+function appendMismatch(file1, file2, path)
 {
     var data = "File header mismatch between " + file1 + " and " + file2 + '\n';
 
     try
     {
-        fs.appendFileSync(sourcePath + "headerMismatch.txt", data, 'utf8');
+        fs.appendFileSync(path + "headerMismatch.txt", data, 'utf8');
         // Get rid of old alert
         $('#mismatchDiv').html("");
 
@@ -102,7 +117,7 @@ function appendMismatch(file1, file2, sourcePath)
 
         // Add message
         var mismatchMessage = $('#mismatchMessage');
-        mismatchMessage.html("There was at least one file with a mismatched header. Check <a href='" + sourcePath + "'headerMismatch.txt' class='alert-link'>this</a> file to view.");
+        mismatchMessage.html("There was at least one file with a mismatched header. A mismatch log was created in the output directory");
     } catch (err)
     {
         // Get rid of old alert
@@ -117,10 +132,12 @@ function appendMismatch(file1, file2, sourcePath)
     }
 }
 
-// This function will return a file's data without the header
-function removeHeader(filePath)
+// This function will return a file's data without the header and any headers within the file (from repeated plays)
+function removeHeader(filePath, header)
 {
     var data = fs.readFileSync(filePath, 'utf8');
+    var regex = new RegExp(header, "g");
+    data = data.replace(regex, '');
     return data.substring(data.indexOf('\n') + 1);
 }
 
